a720ca9d797166d139259e9b9c490031cf789810
Hari-clo <rz246246246@gmail.com>
2025-06-17 18:45:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from backend.model import train_model, predict_salary
+from app.frontend import show_ui
+
+model = train_model("data/Salary_Data.csv")
+
+if __name__ == "__main__":
+    show_ui(model, predict_salary)
