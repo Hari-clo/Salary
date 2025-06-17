0aa34dedcf2860d910449e8ae8d06225b9bc5101
Hari-clo <rz246246246@gmail.com>
2025-06-17 18:55:03 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-def train_model(csv_path):
+def train_model(csv_path="Salary_dataset.csv"):
     data = pd.read_csv(csv_path)
     X = data[['YearsExperience']]
     y = data['Salary']
